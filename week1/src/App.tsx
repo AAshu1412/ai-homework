c30b07dcc471022c5596d4e297ec4f17eaf335cd
AAshu1412 <ashutoshsanjeevmittal@gmail.com>
2024-03-24 18:53:19 +0530

    homework done

--- week1/src/App.tsx
+++ week1/src/App.tsx
@@ -3,14 +3,16 @@ import { GoogleGenerativeAI } from "@google/generative-ai";
 import Markdown from 'react-markdown'
 import { FiLoader } from "react-icons/fi";
 
-const genAI = new GoogleGenerativeAI("AIzaSyBvSswhwR0HD6LfgHLsjGHxtjw7Rgq5wro");
+const api_key=process.env.GEMINI_API_KEY;
+
+const genAI = new GoogleGenerativeAI(api_key as unknown as string);
 const model = genAI.getGenerativeModel({ model: "gemini-pro" });
 
 function App() {
   const [gemini_data, setGemini_data] = useState("");
     const [loading, setLoading] = useState(false);
     const [fetch_gemini_data, setFetch_gemini_data] = useState("");
-  
+
     const handleInput3 = (event:any) => {
       const name = event.target.name;
       // console.log(event.target.value);
@@ -35,11 +37,11 @@ function App() {
           console.log(e);
       }
   }
-  return <div className="flex flex-col items-center justify-center grow">
-      <div className="text-xl">AI Chatbot</div>
-      <div className="text-sm text-black/50 mb-5">Ask away your questions</div>
+  return <div className="flex flex-col items-center justify-center mt-40">
+      <div className="text-6xl font-sem ibold">AI Chatbot</div>
+      <div className="text-lg text-black/50 mb-5">Ask away your questions</div>
 
-      <div className="flex flex-col gap-2 rounded-2xl items-center justify-center">
+      <div className="flex flex-col gap-10 rounded-2xl items-center justify-center">
           <div className="flex flex-row gap-2">
               <input
                   placeholder="Text"
@@ -49,18 +51,19 @@ function App() {
                   onChange={handleInput3}
                   name="gem"
                   id="gem"
-                  className="p-2 rounded-md border border-black/30 outline-none"
+                  className="w-[900px] h-16 p-2 rounded-md border border-black outline-none"
               ></input>
 
               <button
-                  className="border border-black/30 bg-green-300 hover:scale-105 active:scale-95 transition-all duration-200 px-3 rounded-md font-bold text-lg"
+                  className="border border-black bg-orange-500 hover:scale-105 active:scale-95 transition-all duration-200 px-3 rounded-md font-bold text-lg"
                   onClick={fetchDataFromGemini}
               >
-                  GO
+                  Response
               </button>
 
           </div>
-          <div className="overflow-scroll scroll  break-all">
+          -----------------------------
+          <div className="break-all w-[1200px] border border-black p-10">
               {loading?<FiLoader size={26} className="animate-spin"/>:""}
               <Markdown>{fetch_gemini_data}
               </Markdown>
